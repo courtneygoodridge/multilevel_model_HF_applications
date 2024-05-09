a67f20a83605bd1e8da98f4ea360a8d3cf7ade79
courtneygoodridge <c.m.goodridge@leeds.ac.uk>
2024-05-09 16:47:01 +0100

    up to no correlations python script for mixed modelling

--- model_fits_python.py
+++ model_fits_python.py
@@ -16,7 +16,7 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
 # =============================================================================
-# The sklearn has functionality for fitting linear regressions. However, the summary of cofficients, standard errors, and statistical tests is limited.  As such, there is no equivalent to the summary() function in R. Parameter values have to be extracted manually. On explanation for this is that this package is more focused on machine learning and thus using the model for prediction. However, model building requires some inspectin of the model itself before using it for prediction. Hence, we also include a model fitted with the statsmodel package, which provides a more classical statistical approach when summarising. 
+# The sklearn has functionality for fitting linear regressions. However, the summary of cofficients, standard errors, and statistical tests is limited.  As such, there is no equivalent to the summary() function in R. Parameter values have to be extracted manually. On explanation for this is that this package is more focused on machine learning and thus using the model for prediction. However, model building requires some inspectin of the model itself before using it for prediction. Hence, for the main model fitting, we concentrate on statsmodel package which provides a more classical statistical approach when summarising model parameters. 
 # =============================================================================
 
 # Loading Mole et al (2020) data
@@ -55,7 +55,8 @@ results = model_sm.fit()
 print(results.summary())
 
 # =============================================================================
-# Model 2: varying intercept multilevel model - statsmodel
+# Model 2: varying intercept multilevel model - statsmodel variant
+
 # The statsmodel package implementation of linear multilevel models closely follows the implementation used in the lme4 package (outlined in Lindstrom & Bates, JASA, 1988). Hence the results should be identical. The model below has varying intercepts specificed for each member of the ppid group (i.e., an intercept for each participant).
 
 # In the statsmodel package, the random effect for each participant is highlighted in the main table and is titled "Group Var". This refers to the estimated variance (sigma sqaured) of the participant specific randon intercepts. 
@@ -65,5 +66,47 @@ mod_2 = smf.mixedlm("TLC_takeover ~ TLC_failure", data_RTs_cens_NA_removed, grou
 results_mod_2 = mod_2.fit()
 print(results_mod_2.summary())
 
+# =============================================================================
+# Model 3: varying intercept, varying slope model - statsmodel variant
+
+# To add a varying slope with respect to the included predictor, specify a variable using the re_formula argument. The variance parameter for the varying intercept is titled "Group Var", the variance parameter for the varying slope is titled "TLC_failure Var", and the covariance parameter highlighting the correlation between the two must be calculated from these values and the "Group x TLC_failure Cov" values: (-0.005 / sqrt(0.007 * 0.005)) ~ -.83
+
+# =============================================================================
+
+mod_3 = smf.mixedlm("TLC_takeover ~ TLC_failure", data_RTs_cens_NA_removed, groups = data_RTs_cens_NA_removed["ppid"], re_formula = "~TLC_failure")
+results_mod_3 = mod_3.fit()
+print(results_mod_3.summary())
+
+# =============================================================================
+# Model 4: varying intercept, varying slope model (with Goodridge et al, 2024 data) - statsmodel variant
+# =============================================================================
+
+# Loading Goodridge et al (2024) data
+goodridge_2024_dat = pd.read_csv(here('OneDrive - University of Leeds\\ITS\\Multilevel models paper\\Mole et al (2020) data and analysis\\goodridge_2024_dat.csv'))
+
+# python doesn't like dot notation in variable names so here we change the same of the e.norm  to e_norm
+goodridge_2024_dat = goodridge_2024_dat.rename(columns = {"e.norm": "e_norm"})
+
+mod_4 = smf.mixedlm("e_norm ~ n_back * lead", goodridge_2024_dat, groups = goodridge_2024_dat["ppid"], re_formula = "~n_back")
+results_mod_4 = mod_4.fit()
+print(results_mod_4.summary())
+
+# =============================================================================
+# Model 5: maximal model with the Goodridge et al (2024) data - statsmodel variant
+# =============================================================================
+
+mod_5 = smf.mixedlm("e_norm ~ n_back * lead", goodridge_2024_dat, groups = goodridge_2024_dat["ppid"], re_formula = "~n_back * lead")
+results_mod_5 = mod_5.fit()
+print(results_mod_5.summary())
 
 
+# =============================================================================
+# Model 6: model without correlaton parameters using Goodridge et al (2024) data - statsmodel variant
+
+# TO DO - find out what np.ones and np.eye actually mean. This page (https://github.com/statsmodels/statsmodels/issues/1946) suggests that ones means number of fixed effects, whilst eye means number of random effects? Not sure though. 
+# =============================================================================
+
+mod_6 = smf.mixedlm("e_norm ~ n_back * lead", goodridge_2024_dat, groups = goodridge_2024_dat["ppid"], re_formula = "~n_back * lead")
+free = sm.regression.mixed_linear_model.MixedLMParams.from_components(np.ones(4), np.eye(4))
+results_mod_6 = mod_6.fit(free = free)
+print(results_mod_6.summary())
